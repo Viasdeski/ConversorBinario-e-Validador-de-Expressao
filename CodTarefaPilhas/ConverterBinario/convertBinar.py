cc21400f7b855392fec7674896c20a3e657101c8
Viasdeski <86928498+Viasdeski@users.noreply.github.com>
2022-04-10 16:12:54 -0300

    Update convertBinar.py
    Adição de verificador para entrada correta de dados pelos usuários.

--- CodTarefaPilhas/ConverterBinario/convertBinar.py
+++ CodTarefaPilhas/ConverterBinario/convertBinar.py
@@ -1,31 +1,58 @@
+import os
+
 from classes import Binario
 
-bin = Binario()
+converter = True
 
-binario = []
+while converter:
 
-numero = int(input("Qual número deseja converter para binário: "))
+    os.system('clear') # Será criado uma funcao para selecionar o OS e assim atribuir corretamente o comando.
 
-bin.dec2bin(numero)
+    bin = Binario()
+    binario = []
 
-while len(bin) > 0:
-    binario.append(bin.pop())
+    try:
+        numero = input("Qual número deseja converter para binário: ")
+        numero = int(numero)
+    except ValueError:
+        print('Valor inválido. Lmebre-se de digitar um número inteiro')
 
-binario = ''.join(map(str, binario))
-       
-print(f'O número {numero} em binário equivale a: {binario}')
+    bin.dec2bin(numero)
 
+    while len(bin) > 0:
+        binario.append(bin.pop())
 
+    binario = ''.join(map(str, binario))
 
+    print(f'O número {numero} em binário equivale a: {binario}')
 
+    os.system('clear')
 
+    menu = {
+        'Menu': {
+            'pergunta': 'Deseja converter outro número?',
+            'opcoes': {
+                '(1)': 'SIM',
+                '(2)': 'NÃO'
+            }
+        }
+    }
 
+    for chavePerg, chaveResp in menu.items():
+        print(chaveResp['pergunta'])
 
+        for respChave, respValor in chaveResp['opcoes'].items():
+            print(f'{respChave}:{respValor}')
 
+        respUsuario = input('Sua escolha: ')
 
+        os.system('clear')
 
-            
-
-            
-            
-        
+        if respUsuario.isnumeric():
+            if respUsuario == '1':
+                break
+            elif respUsuario == '2':
+                converter = False
+                print('Encerrando conversor...')
+        else:
+            print('O valor deve ser um número inteiro')
